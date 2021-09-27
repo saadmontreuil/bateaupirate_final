e2bd7d47b5330c79a4e9f3b90e755ae42e8a9830
BOUCHAALA-MONTREUIL Saad <saadbouchaalamontreuil@gmail.com>
2021-09-27 11:37:01 +0200

    21

--- handler/paypal.php
+++ handler/paypal.php
@@ -4,7 +4,7 @@
 session_start();
 ?>
 <head>
-    <!-- Add meta tags for mobile and IE -->
+
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 </head>
